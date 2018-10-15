0ccd9f4d78c9f39be714cca1b8cef65a89ad0303
macc <maccavalli@yahoo.com>
2018-10-15 15:45:26 -0400

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://muse:iamamused7@ds019956.mlab.com:19956/mern-shopping1'
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express =require('express'); // the framework
+const mongoose =require('mongoose'); // object modeling for our MongoDB database
+const bodyParser =require('body-parser'); // allow us to take request/data from the body
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
