c3987596fe4aca087ed17d6a4b8ad5c0380d537a
macc <maccavalli@yahoo.com>
2018-10-15 18:05:20 -0400

    Setup GET items route

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,4 +4,13 @@ const router = express.Router();
 // Item Model
 const Item = require('../../models/Item');
 
+// @route   GET api/items
+// @desc    GET All Item
+// @access  Public
+router.get('/', (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items))
+});
+
 module.exports = router;
