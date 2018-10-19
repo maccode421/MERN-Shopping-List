d28085925935c457ba8ddc23df9c23d8fbc1eda9
macc <maccavalli@yahoo.com>
2018-10-19 17:53:34 -0400

    Set up static folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express =require('express'); // the framework
-const mongoose =require('mongoose'); // object modeling for our MongoDB database
-const bodyParser =require('body-parser'); // allow us to take request/data from the body
+const express = require('express'); // the framework
+const mongoose = require('mongoose'); // object modeling for our MongoDB database
+const bodyParser = require('body-parser'); // allow us to take request/data from the body
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -19,6 +20,16 @@ mongoose.connect(db, { useNewUrlParser: true }) // Adding new mongo url parser
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
